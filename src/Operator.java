2f8482eb25620886381ccf15024eb075badac581
Mark Barnes <barnemar002@warren.k12.in.us>
2024-08-09 13:44:10 -0400

    update

--- src/Operator.java
+++ src/Operator.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Operator {
+public class Operator {
+    public static void main(String[] args) {
+        double val1 = 20.00;
+        double val2 = 80.7;
+        double val3 = (val1 + val2) * 100.00;
+
+        System.out.println("My values total = " + val3);
+        double remainder = val3 % 40.00;
+
+        boolean remainderOrNot = (remainder == 0) ? true : false;
+        System.out.println("remainderOrNot = " + remainderOrNot);
+
+        String s = (remainderOrNot) ? "Has remainder" : "Doesn`t have remainder";
+        System.out.println(s);
+
+        if(!remainderOrNot) {
+            System.out.println("Got some remainder");
+        }
+    }
 }
